d5f293207a19167bffc64b7dc3d8d981076496e1
Quan (Uri) K.LE <uri@enclave.vn>
2020-12-15 11:00:26 +0700

    Update Categories name

--- lib/models/categories.dart
+++ lib/models/categories.dart
@@ -1,12 +1,12 @@
-class Category {
+class Categories {
   final String id, title, image;
   final int numofProducts;
 
-  Category({this.id, this.title, this.image, this.numofProducts});
+  Categories({this.id, this.title, this.image, this.numofProducts});
 
   // Create Json Category fie
-  factory Category.fromJson(Map<String, dynamic> json) {
-    return Category(
+  factory Categories.fromJson(Map<String, dynamic> json) {
+    return Categories(
       id: json["id"],
       title: json["title"],
       image: json["image"],
@@ -15,7 +15,7 @@ class Category {
   }
 }
 
-Category category = Category(
+Categories category = Categories(
   id: "1",
   title: "Armchair",
   image: "https://i.imgur.com/JqKDdxj.png",
