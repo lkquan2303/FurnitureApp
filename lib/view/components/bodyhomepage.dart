ed53dedc18aa9e8f0d969ee9499bb08d84414898
Quan (Uri) K.LE <uri@enclave.vn>
2020-12-17 16:50:52 +0700

    Update Components

--- lib/view/components/bodyhomepage.dart
+++ lib/view/components/bodyhomepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:furniture_app/components/title_text.dart';
 import 'package:furniture_app/models/categories.dart';
 import 'package:furniture_app/services/fetchCategories.dart';
+import 'categories.dart';
 import 'categories_card.dart';
 
 class BodyHomepage extends StatelessWidget {
@@ -23,31 +24,12 @@ class BodyHomepage extends StatelessWidget {
                 ? Categories(
                     categories: snapshot.data,
                   )
-                : Center(child: Image.asset("assets/ripple.gif")),
+                : Center(
+                    child: Image.asset("assets/ripple.gif"),
+                  ),
           )
         ],
       ),
-    );
-  }
-}
-
-class Categories extends StatelessWidget {
-  const Categories({
-    Key key,
-    this.categories,
-  }) : super(key: key);
-  final List<Category> categories;
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-          children: List.generate(
-              categories.length,
-              (index) => Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: CategoryCard(categories: categories[index]),
-                  ))),
     );
   }
 }
---
+++ lib/view/components/categories.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_app/models/categories.dart';
+
+import 'categories_card.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({
+    Key key,
+    this.categories,
+  }) : super(key: key);
+  final List<Category> categories;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+          children: List.generate(
+              categories.length,
+              (index) => Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: CategoryCard(categories: categories[index]),
+                  ))),
+    );
+  }
+}
